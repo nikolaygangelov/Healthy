80f6547320f87613b768d133e070b7635a566fa1
nikolaygangelov <nikolay.g.angelov@gmail.com>
2024-03-07 12:30:40 +0200

    ContactsDetailsComponent added

---
+++ Healthy/Components/ContactsDetailsComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Healthy.Components
+{
+	public class ContactsDetailsComponent : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return await Task.FromResult<IViewComponentResult>(View("ContactsDetails"));
+		}
+	}
+}
