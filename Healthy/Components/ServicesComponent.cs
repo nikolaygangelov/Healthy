b5384d0e89673852e3a19ab6233fccbcc5fe2766
nikolaygangelov <nikolay.g.angelov@gmail.com>
2024-03-07 12:57:35 +0200

    ServicesComponent added

---
+++ Healthy/Components/ServicesComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Healthy.Components
+{
+	public class ServicesComponent : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return await Task.FromResult<IViewComponentResult>(View("Services"));
+		}
+	}
+}
