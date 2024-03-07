cfbcdb2ff5bb3cf6033160a1c6ce8b5794cb46ff
nikolaygangelov <nikolay.g.angelov@gmail.com>
2024-03-07 12:39:19 +0200

    ReferencesComponent added

---
+++ Healthy/Components/ReferencesComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Healthy.Components
+{
+	public class ReferencesComponent : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return await Task.FromResult<IViewComponentResult>(View("References"));
+		}
+	}
+}
